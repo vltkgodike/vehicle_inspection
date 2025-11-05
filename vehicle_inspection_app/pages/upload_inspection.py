dc99dc4874770482406ef5d60b33fd649c8fb2a7
vltkgodike <mahesh.g@valkontek.com>
2025-11-05 14:37:34 +0530

    Update model URLs and enhance detection process

--- vehicle_inspection_app/pages/upload_inspection.py
+++ vehicle_inspection_app/pages/upload_inspection.py
@@ -19,7 +19,7 @@ st.title("📷 Upload Inspection Image")
 # ✅ Define Model URLs (Hugging Face)
 # ---------------------------#
 MODEL_URLS = {
-    "Scratch": "https://huggingface.co/mahigodike/AVIS/resolve/main/scratch_model_v2.pt",
+    "Scratch": "https://huggingface.co/mahigodike/AVIS/resolve/main/best_scratch_v3m.pt",
     "Dent": "https://huggingface.co/mahigodike/AVIS/resolve/main/best_model_dentw.pt",
     "Corrosion": "https://huggingface.co/mahigodike/AVIS/resolve/main/corrosion.pt",
     "Windshield": "https://huggingface.co/mahigodike/AVIS/resolve/main/windshield.pt"
@@ -144,3 +144,4 @@ if uploaded_file:
 
         time.sleep(2)
         st.switch_page("pages/report.py")
+
